b73ce17f4fbffad0336103551fcf168d83a96dcd
Solomon <sam0162003@163.com>
2022-10-11 11:24:31 +0800

    [修改内容]：添加：1、履职计划表实体类、mapper类、业务类和控制器 [修改时间]：2022-10-11 [ 作  者 ]：兰宇铧

---
+++ src/main/java/com/fssy/shareholder/management/controller/system/performance/employee/EntryCasMergeController.java
@@ -0,0 +1,25 @@
+/**   
+ * ------------------------修改日志---------------------------------
+ * 修改人			修改日期			修改内容
+ */
+package com.fssy.shareholder.management.controller.system.performance.employee;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * *****业务部门： 绩效科 *****数据表中文名： 员工履职表 *****数据表名：
+ * bs_performance_employee_entry_cas_merge *****数据表作用：
+ * 员工每月月初填写的履职情况计划表，以及在计划上写回顾，评价都在这里 *****变更记录： 时间 变更人 变更内容 20220915 兰宇铧 初始设计
+ * 前端控制器
+ * </p>
+ *
+ * @author Solomon
+ * @since 2022-10-11
+ */
+@Controller
+@RequestMapping("/fssy.shareholder.management/entry-cas-merge")
+public class EntryCasMergeController {
+
+}
---
+++ src/main/java/com/fssy/shareholder/management/service/system/impl/performance/employee/EntryCasMergeServiceImpl.java
@@ -0,0 +1,27 @@
+/**   
+ * ------------------------修改日志---------------------------------
+ * 修改人			修改日期			修改内容
+ */
+package com.fssy.shareholder.management.service.system.impl.performance.employee;
+
+import com.fssy.shareholder.management.pojo.system.performance.employee.EntryCasMerge;
+import com.fssy.shareholder.management.mapper.system.performance.employee.EntryCasMergeMapper;
+import com.fssy.shareholder.management.service.system.performance.employee.EntryCasMergeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * *****业务部门： 绩效科 *****数据表中文名： 员工履职表 *****数据表名：
+ * bs_performance_employee_entry_cas_merge *****数据表作用：
+ * 员工每月月初填写的履职情况计划表，以及在计划上写回顾，评价都在这里 *****变更记录： 时间 变更人 变更内容 20220915 兰宇铧 初始设计
+ * 服务实现类
+ * </p>
+ *
+ * @author Solomon
+ * @since 2022-10-11
+ */
+@Service
+public class EntryCasMergeServiceImpl extends ServiceImpl<EntryCasMergeMapper, EntryCasMerge> implements EntryCasMergeService {
+
+}
