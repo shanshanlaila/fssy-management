3b2b4a1d1fe1bfc5f0a2a7c8f372cb47ea5923e2
施镇脑 <804854993@qq.com>
2022-10-25 22:23:39 +0800

    【修改内容】1、经营指标库数据的导入和导出（不包含修改和删除）  2、经营管理年度项目指标的导入和导出（不包含修改和删除）  3、经营管理月度目标值导出  4、经营管理月度实绩导入 【修改时间】2022/10/25 【修改人】施镇脑

---
+++ src/main/java/com/fssy/shareholder/management/mapper/system/manage/ManageKpiYearMapper.java
@@ -0,0 +1,18 @@
+package com.fssy.shareholder.management.mapper.system.manage;
+
+import com.fssy.shareholder.management.mapper.manage.common.MyBaseMapper;
+import com.fssy.shareholder.management.pojo.system.manage.ManageKpiYear;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 经理人年度KPI Mapper 接口
+ * </p>
+ *
+ * @author Shizn
+ * @since 2022-10-20
+ */
+@Mapper
+public interface ManageKpiYearMapper extends MyBaseMapper<ManageKpiYear> {
+
+}
