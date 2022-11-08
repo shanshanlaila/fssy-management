db9a0e212d4957e873de51d4711cc24743e94a6f
张泽鹏 <zzp13369137498@163.com>
2022-11-08 17:17:03 +0800

    【修改内容】 ①完善了经理人kpi的导入导出，展示增加了职位类别及是否总经理字段 ②完善了经理人绩效分数计算，修改了展示页面职位类别下拉框 【修改时间】 2022-11-08 【修改人】 张泽鹏

--- src/main/java/com/fssy/shareholder/management/service/common/override/ViewManagerKpiYearSheetOutputService.java
+++ src/main/java/com/fssy/shareholder/management/service/common/override/ViewManagerKpiYearSheetOutputService.java
@@ -60,27 +60,27 @@ public class ViewManagerKpiYearSheetOutputService extends SheetOutputService {
                 .getFormat("yyyy-MM-dd");
         dateStyle.setDataFormat(dateFormat);
 
-//        //1.设置合并单元格
-//        //标题
-//        CellRangeAddress titleRange = new CellRangeAddress(0,0,
-//                SheetService.columnToIndex("A"),
-//                SheetService.columnToIndex("AA"));
-//        //副标题：填报企业
-//        CellRangeAddress setCompanyName = new CellRangeAddress(1,1,
-//                SheetService.columnToIndex("A"),
-//                SheetService.columnToIndex("B"));
-//        //副标题：企业名称
-//        CellRangeAddress getCompanyName = new CellRangeAddress(1,1,
-//                SheetService.columnToIndex("C"),
-//                SheetService.columnToIndex("E"));
-//        //副标题：年份
-//        CellRangeAddress setYear = new CellRangeAddress(1,1,
-//                SheetService.columnToIndex("F"),
-//                SheetService.columnToIndex("G"));
-//        //副标题：获取年份
-//        CellRangeAddress getYear = new CellRangeAddress(1,1,
-//                SheetService.columnToIndex("H"),
-//                SheetService.columnToIndex("I"));
+        //1.设置合并单元格
+        //标题
+        CellRangeAddress titleRange = new CellRangeAddress(0,0,
+                SheetService.columnToIndex("A"),
+                SheetService.columnToIndex("Q"));
+        //副标题：填报企业
+        CellRangeAddress setCompanyName = new CellRangeAddress(1,1,
+                SheetService.columnToIndex("A"),
+                SheetService.columnToIndex("B"));
+        //副标题：企业名称
+        CellRangeAddress getCompanyName = new CellRangeAddress(1,1,
+                SheetService.columnToIndex("C"),
+                SheetService.columnToIndex("E"));
+        //副标题：年份
+        CellRangeAddress setYear = new CellRangeAddress(1,1,
+                SheetService.columnToIndex("F"),
+                SheetService.columnToIndex("G"));
+        //副标题：获取年份
+        CellRangeAddress getYear = new CellRangeAddress(1,1,
+                SheetService.columnToIndex("H"),
+                SheetService.columnToIndex("I"));
 //        //副标题：月份
 //        CellRangeAddress setMonth = new CellRangeAddress(1,1,
 //                SheetService.columnToIndex("J"),
@@ -91,42 +91,42 @@ public class ViewManagerKpiYearSheetOutputService extends SheetOutputService {
 //                SheetService.columnToIndex("M"));
 //
 //        sheet.addMergedRegion(getMonth);
-//        sheet.addMergedRegion(setYear);
-//        sheet.addMergedRegion(getYear);
+        sheet.addMergedRegion(setYear);
+        sheet.addMergedRegion(getYear);
 //        sheet.addMergedRegion(setMonth);
-//        sheet.addMergedRegion(getCompanyName);
-//        sheet.addMergedRegion(setCompanyName);
-//        sheet.addMergedRegion(titleRange);
+        sheet.addMergedRegion(getCompanyName);
+        sheet.addMergedRegion(setCompanyName);
+        sheet.addMergedRegion(titleRange);
 
         // 2.写内容
         // 获取数据
         @SuppressWarnings("unchecked")
         Map<String, Object> data = (Map<String, Object>) list.get(0);
-//        // 标题
-//        XSSFCell titleCell = sheet.createRow(0)
-//                .createCell(SheetService.columnToIndex("A"));
-//        titleCell.setCellStyle(style);
-//        titleCell.setCellValue("年度经营计划管理项目月度报表");
+        // 标题
+        XSSFCell titleCell = sheet.createRow(0)
+                .createCell(SheetService.columnToIndex("A"));
+        titleCell.setCellStyle(style);
+        titleCell.setCellValue("经理人年度KPI项目指标报表");
         //填报企业
-        XSSFRow twoRow = sheet.createRow(0);
+        XSSFRow twoRow = sheet.createRow(1);
         XSSFCell getCompanyNameCell = twoRow
                 .createCell(SheetService.columnToIndex("A"));
         getCompanyNameCell.setCellStyle(style);
         getCompanyNameCell.setCellValue("填报企业");
         //填报企业
         XSSFCell setCompanyNameCell = twoRow
-                .createCell(SheetService.columnToIndex("B"));
+                .createCell(SheetService.columnToIndex("C"));
         setCompanyNameCell.setCellStyle(style);
         setCompanyNameCell.setCellValue(ObjectUtils.isEmpty(data.get("companyName")) ? ""
                 :data.get("companyName").toString());
         //年份
         XSSFCell yearCell = twoRow
-                .createCell(SheetService.columnToIndex("C"));
+                .createCell(SheetService.columnToIndex("F"));
         yearCell.setCellStyle(style);
         yearCell.setCellValue("年份");
         //年份
         XSSFCell setYearCell = twoRow
-                .createCell(SheetService.columnToIndex("D"));
+                .createCell(SheetService.columnToIndex("H"));
         setYearCell.setCellStyle(style);
         setYearCell.setCellValue(ObjectUtils.isEmpty(data.get("year")) ? ""
                 :data.get("year").toString());
@@ -143,14 +143,14 @@ public class ViewManagerKpiYearSheetOutputService extends SheetOutputService {
 //                :data.get("month").toString());
 
 //        // 3.设置合并单元格边框
-//        setRegionBorder(titleRange, sheet);
-//        setRegionBorder(setCompanyName, sheet);
-//        setRegionBorder(getCompanyName, sheet);
-//        setRegionBorder(setYear,sheet);
-//        setRegionBorder(getYear,sheet);
+        setRegionBorder(titleRange, sheet);
+        setRegionBorder(setCompanyName, sheet);
+        setRegionBorder(getCompanyName, sheet);
+        setRegionBorder(setYear,sheet);
+        setRegionBorder(getYear,sheet);
 //        setRegionBorder(setMonth,sheet);
 //        setRegionBorder(getMonth,sheet);
         //从excel表中的第2行开始读
-        return 1;
+        return 2;
     }
 }
