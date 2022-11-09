c5772743f247ea3f6df9987f50540381ae3d5585
张泽鹏 <zzp13369137498@163.com>
2022-11-09 14:50:25 +0800

    【修改内容】完善了经理人分数的计算 【修改时间】2022-11-09 【修改人】张泽鹏

--- src/main/java/com/fssy/shareholder/management/controller/system/performance/manage_kpi/ViewManagerKpiMonthController.java
+++ src/main/java/com/fssy/shareholder/management/controller/system/performance/manage_kpi/ViewManagerKpiMonthController.java
@@ -5,11 +5,18 @@ import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fssy.shareholder.management.annotation.RequiredLog;
 import com.fssy.shareholder.management.pojo.common.SysResult;
+import com.fssy.shareholder.management.pojo.system.config.Attachment;
+import com.fssy.shareholder.management.pojo.system.config.ImportModule;
 import com.fssy.shareholder.management.pojo.system.performance.manage_kpi.ManagerKpiScoreOld;
 import com.fssy.shareholder.management.service.common.SheetOutputService;
 import com.fssy.shareholder.management.service.common.override.ManageKpiMonthPerformanceSheetOutputService;
+import com.fssy.shareholder.management.service.system.config.AttachmentService;
+import com.fssy.shareholder.management.service.system.config.ImportModuleService;
 import com.fssy.shareholder.management.service.system.performance.manage_kpi.ManagerKpiScoreServiceOld;
 import com.fssy.shareholder.management.service.system.performance.manage_kpi.ViewManagerKpiMonthService;
+import com.fssy.shareholder.management.tools.common.FileAttachmentTool;
+import com.fssy.shareholder.management.tools.common.InstandTool;
+import com.fssy.shareholder.management.tools.constant.CommonConstant;
 import com.fssy.shareholder.management.tools.exception.ServiceException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +24,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -33,12 +42,102 @@ import java.util.*;
 @Controller
 @RequestMapping("/system/performance/manager_kpi/view-manager-kpi-month-score")
 public class ViewManagerKpiMonthController {
+
     @Autowired
     private ViewManagerKpiMonthService viewManagerKpiMonthService;
     @Autowired
     private ManagerKpiScoreServiceOld managerKpiScoreService;
+    @Autowired
+    private AttachmentService attachmentService;
+    @Autowired
+    private FileAttachmentTool fileAttachmentTool;
+    @Autowired
+    private ImportModuleService importModuleService;
+
+    /**
+     * 返回附件上传页面
+     *
+     * @param model model对象
+     * @return 页面
+     */
+    @RequiredLog("附件上传")
+    @RequiresPermissions("system:performance:manager_kpi:view-manager-kpi-month-score:index")
+    @GetMapping("index")
+    public String materialDataAttachmentIndex(Model model) {
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        String importDateStart = sdf.format(date);
+        model.addAttribute("importDateStart", importDateStart);
+        // 查询导入场景
+        Map<String, Object> params = new HashMap<>();
+        params.put("noteEq", "经理人KPI分数表");
+        List<ImportModule> importModules = importModuleService
+                .findImportModuleDataListByParams(params);
+        if (org.springframework.util.ObjectUtils.isEmpty(importModules)) {
+            throw new ServiceException(String.format("描述为【%s】的导入场景未维护，不允许查询", "经理人KPI分数表"));
+        }
+        model.addAttribute("module", importModules.get(0).getId());
+        return "/system/performance/manager_kpi/view-manager-kpi-month-score/view-manager-kpi-month-score-attachment-list";
+    }
+
+    /**
+     * 附件上传
+     *
+     * @param file       前台传来的附件数据
+     * @param attachment 附件表实体类
+     * @return 附件ID
+     */
+    @PostMapping("uploadFile")
+    @RequiredLog("经理人KPI分数附件上传")
+    @ResponseBody
+    public SysResult uploadFile(@RequestParam("file") MultipartFile file, Attachment attachment) {
+//        //判断是否选择对应年份
+//        Map<String, Object> params = getParams(request);
+//        String year = (String) params.get("year");
+//        if (org.springframework.util.ObjectUtils.isEmpty(params.get("year"))) {
+//            throw new ServiceException("未选择年份，导入失败");
+//        }
+        // 保存附件
+        Calendar calendar = Calendar.getInstance();
+        attachment.setImportDate(calendar.getTime());//设置时间
+        // 查询导入场景对象
+        ImportModule module = importModuleService
+                .findById(InstandTool.integerToLong(attachment.getModule()));
+        if (org.springframework.util.ObjectUtils.isEmpty(module)) {
+            throw new ServiceException(
+                    String.format("序号为【%s】的导入场景未维护，不允许导入", attachment.getModule()));
+        }
+        Attachment result = fileAttachmentTool.storeFileToModule(file, module, // 上传文件至数据库的类别，主要目的是分类展示
+                attachment);
 
 
+        try {
+            // 读取附件并保存数据
+            Map<String, Object> resultMap = managerKpiScoreService.readManagerKpiScoreOldDataSource(result);
+            if (Boolean.parseBoolean(resultMap.get("failed").toString())) {// "failed" : true
+                attachmentService.changeImportStatus(CommonConstant.IMPORT_RESULT_SUCCESS,
+                        result.getId().toString(), String.valueOf(resultMap.get("content")));
+                return SysResult.build(200, "表中有空值，未导入成功的数据请看附件导入列表页面！请重新导入失败的数据");
+            } else {
+                // 修改附件为导入成功
+                attachmentService.changeImportStatus(CommonConstant.IMPORT_RESULT_SUCCESS,
+                        result.getId().toString(), "导入成功");// 表格备注中的内容
+                return SysResult.ok();
+            }
+        } catch (ServiceException e) {
+            // 修改附件的导入状态为失败
+            attachmentService.changeImportStatus(CommonConstant.IMPORT_RESULT_FAILED, result.getId().toString(), e.getMessage());
+            throw new ServiceException("计划导入失败，失败原因请查看附件列表备注描述，更改后请重新导入数据");
+        } catch (Exception e) {
+            // 修改附件导入状态为失败
+            attachmentService.changeImportStatus(CommonConstant.IMPORT_RESULT_FAILED,
+                    result.getId().toString());
+            e.printStackTrace();
+            throw e;
+        }
+    }
     /**
      * 经理人月度KPI分数管理界面
      * @param model
@@ -105,58 +204,7 @@ public class ViewManagerKpiMonthController {
         return SysResult.build(500, "分数生成失败");
     }
 
-//    /**
-//     * 以主键删除经理人分数记录
-//     * @param managerName
-//     * @return true/false
-//     */
-//    @DeleteMapping("{managerName}")
-//    @ResponseBody
-//    public SysResult delete(@PathVariable(value = "managerName") String managerName) {
-//        System.out.println("--------------");
-//        boolean result = managerKpiScoreService.deleteStudentById(id);
-//        if (result) {
-//            return SysResult.ok();
-//        }
-//        return SysResult.build(500, "删除数据失败");
-//    }
-    /**
-     * excel 导出
-     * @param request 请求
-     * @param response 响应
-     */
-    @RequiredLog("数据导出")
-    @GetMapping("downloadForCharge")
-    public void downloadForCharge(HttpServletRequest request, HttpServletResponse response) {
-        Map<String,Object> params = getParams(request);
-        //Sql语句
-        params.put("select","year,managerName,companyName,position,generalManager,month,scoreAdjust,scoreAuto," +
-                "businessScore,incentiveScore,difficultyCoefficient,generalManagerScore");
-        //查询
-        List<Map<String,Object>> viewManagerKpiYearDataByParams = viewManagerKpiMonthService.findViewManagerKpiMonthMapDataByParams(params);
-        LinkedHashMap<String,String> fieldMap = new LinkedHashMap<>();
 
-        //需要改变背景的格子
-        fieldMap.put("year", "年份");
-        fieldMap.put("managerName", "经理人姓名");
-        fieldMap.put("companyName", "企业名称");
-        fieldMap.put("position", "职务");
-        fieldMap.put("generalManager", "是否总经理");
-        fieldMap.put("month", "月份");
-        fieldMap.put("scoreAuto", "系统生成分数");
-        fieldMap.put("scoreAdjust", "人工调整分数");
-        fieldMap.put("businessScore", "经营绩效分数");
-        fieldMap.put("incentiveScore", "激励约束分数");
-        fieldMap.put("difficultyCoefficient", "难度系数");
-        fieldMap.put("generalManagerScore", "总经理经营绩效分数");
-        //标识字符串的列
-        List<Integer> strList = Arrays.asList(0,1,2,3,4,5,6,7,8,9,10,11);
-        SheetOutputService sheetOutputService = new ManageKpiMonthPerformanceSheetOutputService();
-        if (org.apache.commons.lang3.ObjectUtils.isEmpty(viewManagerKpiYearDataByParams)) {
-            throw new ServiceException("未查出数据");
-        }
-        sheetOutputService.exportNum("经理人月度KPI分数表", viewManagerKpiYearDataByParams, fieldMap, response, strList, null);
-    }
 
     //获取数据库里的数据
     private Map<String, Object> getParams(HttpServletRequest request) {
--- src/main/java/com/fssy/shareholder/management/service/system/impl/performance/manage_kpi/ManagerKpiScoreServiceImplOld.java
+++ src/main/java/com/fssy/shareholder/management/service/system/impl/performance/manage_kpi/ManagerKpiScoreServiceImplOld.java
@@ -2,14 +2,28 @@ package com.fssy.shareholder.management.service.system.impl.performance.manage_k
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fssy.shareholder.management.pojo.system.config.Attachment;
+import com.fssy.shareholder.management.pojo.system.performance.manage_kpi.ManageKpiLib;
+import com.fssy.shareholder.management.pojo.system.performance.manage_kpi.ManageKpiMonthPerformance;
 import com.fssy.shareholder.management.pojo.system.performance.manage_kpi.ManagerKpiScoreOld;
 import com.fssy.shareholder.management.mapper.system.performance.manage_kpi.ManagerKpiScoreMapperOld;
 import com.fssy.shareholder.management.pojo.system.performance.manage_kpi.ViewManagerKpiMonth;
+import com.fssy.shareholder.management.service.common.SheetService;
 import com.fssy.shareholder.management.service.system.performance.manage_kpi.ManagerKpiScoreServiceOld;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fssy.shareholder.management.tools.exception.ServiceException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,11 +38,18 @@ import java.util.Map;
 @Service
 public class ManagerKpiScoreServiceImplOld extends ServiceImpl<ManagerKpiScoreMapperOld, ManagerKpiScoreOld> implements ManagerKpiScoreServiceOld {
 
+    @Autowired
+    private SheetService sheetService;
 
     @Autowired
     private ManagerKpiScoreMapperOld managerKpiScoreMapper;
 
-
+    /**
+     * 通过查询条件 分页 查询列表
+     *
+     * @param params 查询条件
+     * @return 分页数据
+     */
     @Override
     public Page<ManagerKpiScoreOld> findViewManagerKpiMonthDataListPerPageByParams(Map<String, Object> params) {
         QueryWrapper<ManagerKpiScoreOld> queryWrapper = getQueryWrapper(params);
@@ -36,6 +57,145 @@ public class ManagerKpiScoreServiceImplOld extends ServiceImpl<ManagerKpiScoreMa
         int page = (int)params.get("page");
         Page<ManagerKpiScoreOld> myPage = new Page<>(page,limit);
         return managerKpiScoreMapper.selectPage(myPage, queryWrapper);
+    }
+
+    /**
+     * 设置失败的内容
+     *
+     * @param result 结果map
+     * @param append 导入失败的原因
+     */
+    private void setFailedContent(Map<String, Object> result, String append) {
+        String content = result.get("content").toString();
+        if (ObjectUtils.isEmpty(content)) {
+            result.put("content", append);
+        } else {
+            result.put("content", content + "," + append);
+        }
+        result.put("failed", true);
+    }
+
+    /**
+     * 导入附件
+     * @param attachment 附件
+     * @return
+     */
+    @Override
+    @Transactional
+    public Map<String, Object> readManagerKpiScoreOldDataSource(Attachment attachment) {
+        // 返回消息
+        Map<String, Object> result = new HashMap<>();
+        result.put("content", "");
+        result.put("failed", false);
+        // 读取附件
+        sheetService.load(attachment.getPath(), attachment.getFilename()); // 根据路径和名称读取附件
+        // 读取表单
+        sheetService.readByName("经理人KPI分数表"); //根据表单名称获取该工作表单
+        // 获取表单数据
+        Sheet sheet = sheetService.getSheet();
+        if (ObjectUtils.isEmpty(sheet)) {
+            throw new ServiceException("表单【经理人KPI分数表】不存在，无法读取数据，请检查");
+        }
+
+        // 获取单价列表数据
+        Row row;
+        List<ManagerKpiScoreOld> managerKpiScoreOlds = new ArrayList<>(); //实体类集合，用于后面的批量写入数据库
+        // 2022-06-01 从决策系统导出数据，存在最后几行为空白数据，导致报数据越界问题，这里的长度由表头长度控制
+        short maxSize = sheet.getRow(0).getLastCellNum();//列数(表头长度)
+        // 循环总行数(不读表的标题，从第1行开始读)
+        for (int j = 1; j <= sheet.getLastRowNum(); j++) {// getPhysicalNumberOfRows()此方法不会将空白行计入行数
+            List<String> cells = new ArrayList<>();// 每一行的数据用一个list接收
+            row = sheet.getRow(j);// 获取第j行
+            // 获取一行中有多少列 Row：行，cell：列
+            // 循环row行中的每一个单元格
+            for (int k = 0; k < maxSize; k++) {
+                // 如果这单元格为空，就写入空
+                if (row.getCell(k) == null) {
+                    cells.add("");
+                    continue;
+                }
+                // 处理单元格读取到公式的问题
+                if (row.getCell(k).getCellType() == CellType.FORMULA) {
+                    row.getCell(k).setCellType(CellType.STRING);
+                    String res = row.getCell(k).getStringCellValue();
+                    cells.add(res);
+                    continue;
+                }
+                Cell cell = row.getCell(k);
+                String res = sheetService.getValue(cell).trim();// 获取单元格的值
+                cells.add(res);// 将单元格的值写入行
+            }
+            // 导入结果写入列
+            Cell cell = row.createCell(SheetService.columnToIndex("R"));// 报错信息上传到excel R列（暂未实现）
+            String year = cells.get(SheetService.columnToIndex("A"));
+            String managerName = cells.get(SheetService.columnToIndex("B"));
+            String companyName = cells.get(SheetService.columnToIndex("C"));
+            String position = cells.get(SheetService.columnToIndex("D"));
+            String generalManager = cells.get(SheetService.columnToIndex("E"));
+            String month = cells.get(SheetService.columnToIndex("F"));
+            String businessScore = cells.get(SheetService.columnToIndex("G"));
+            String incentiveScore = cells.get(SheetService.columnToIndex("H"));
+            String difficultyCoefficient = cells.get(SheetService.columnToIndex("I"));
+            String generalManagerScore = cells.get(SheetService.columnToIndex("J"));
+            String scoreAdjust = cells.get(SheetService.columnToIndex("K"));
+            String scoreAuto = cells.get(SheetService.columnToIndex("L"));
+            String advantageAnalyze = cells.get(SheetService.columnToIndex("M"));
+            String disadvantageAnalyze = cells.get(SheetService.columnToIndex("N"));
+            String riskDesc = cells.get(SheetService.columnToIndex("O"));
+            String respDepartment = cells.get(SheetService.columnToIndex("P"));
+            String groupImproveAction = cells.get(SheetService.columnToIndex("Q"));
+            // 检查必填项
+            if (ObjectUtils.isEmpty(companyName)) {
+                setFailedContent(result, String.format("第%s行的经理人姓名是空的", j + 1));
+                cell.setCellValue("经理人姓名是空的");
+                throw new ServiceException("表中有空值");
+            }
+
+            //构建实体类
+            ManagerKpiScoreOld managerKpiScoreOld = new ManagerKpiScoreOld();
+            managerKpiScoreOld.setYear(Integer.valueOf(year));
+            managerKpiScoreOld.setManagerName(managerName);
+            managerKpiScoreOld.setCompanyName(companyName);
+            managerKpiScoreOld.setPosition(position);
+            managerKpiScoreOld.setGeneralManager(generalManager);
+            managerKpiScoreOld.setMonth(Integer.valueOf(month));
+            managerKpiScoreOld.setBusinessScore(new BigDecimal(businessScore));
+            managerKpiScoreOld.setIncentiveScore(new BigDecimal(incentiveScore));
+            if (!ObjectUtils.isEmpty(difficultyCoefficient)) {
+                managerKpiScoreOld.setDifficultyCoefficient(new BigDecimal(difficultyCoefficient));
+            }
+            if (!ObjectUtils.isEmpty(generalManagerScore)) {
+                managerKpiScoreOld.setGeneralManagerScore(new BigDecimal(generalManagerScore));
+            }
+            managerKpiScoreOld.setScoreAuto(new BigDecimal(scoreAuto));
+            managerKpiScoreOld.setScoreAdjust(new BigDecimal(scoreAdjust));
+            managerKpiScoreOld.setAdvantageAnalyze(advantageAnalyze);
+            managerKpiScoreOld.setDisadvantageAnalyze(disadvantageAnalyze);
+            managerKpiScoreOld.setRiskDesc(riskDesc);
+            managerKpiScoreOld.setRespDepartment(respDepartment);
+            managerKpiScoreOld.setGroupImproveAction(groupImproveAction);
+
+            // 根据id进行判断，存在则更新，不存在则新增
+            saveOrUpdate(managerKpiScoreOld);
+            cell.setCellValue("导入成功");
+
+        }
+
+        sheetService.write(attachment.getPath(), attachment.getFilename());// 写入excel表
+        return result;
+    }
+
+
+    /**
+     * 通过查询条件查询履职计划map数据，用于导出
+     *
+     * @param params 查询条件
+     * @return 数据列表
+     */
+    @Override
+    public List<Map<String, Object>> findManagerKpiScoreOldDataByParams(Map<String, Object> params) {
+        QueryWrapper<ManagerKpiScoreOld> queryWrapper = getQueryWrapper(params);
+        return managerKpiScoreMapper.selectMaps(queryWrapper);
     }
 
     /**
