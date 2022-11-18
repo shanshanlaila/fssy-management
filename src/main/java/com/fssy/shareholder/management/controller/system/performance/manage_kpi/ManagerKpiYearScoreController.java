65b5ed50523c2b7daa1a3727625e1e187f1fbb3f
张泽鹏 <zzp13369137498@163.com>
2022-11-18 20:31:18 +0800

    【修改内容】完善了经理人分数计算，实现了月度推移报表的展示功能 【修改时间】2022-11-18 【修改人】张泽鹏

--- src/main/java/com/fssy/shareholder/management/controller/system/performance/manage_kpi/ManagerKpiYearScoreController.java
+++ src/main/java/com/fssy/shareholder/management/controller/system/performance/manage_kpi/ManagerKpiYearScoreController.java
@@ -1,84 +1,75 @@
+/**
+ * ------------------------修改日志---------------------------------
+ * 修改人       修改日期         修改内容
+ */
 package com.fssy.shareholder.management.controller.system.performance.manage_kpi;
-
 
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fssy.shareholder.management.annotation.RequiredLog;
-import com.fssy.shareholder.management.service.common.SheetOutputService;
+import com.fssy.shareholder.management.pojo.system.performance.manage_kpi.ManagerKpiScoreOld;
 import com.fssy.shareholder.management.service.system.performance.manage_kpi.ManagerKpiScoreServiceOld;
-import com.fssy.shareholder.management.tools.exception.ServiceException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * <p>
- * 经理人绩效分数表		【修改内容】增加了年度关联ID字段	【修改时间】2022-10-27	【修改人】张泽鹏 前端控制器
- * 暂时没有使用
- * </p>
- *
- * @author zzp
- * @since 2022-10-31
+ * @author Shizn
+ * @ClassName: ManagerKpiYearScoreController
+ * @Description: 经理人年度KPI绩效分推移 * @date 2022/11/18 0018 10:27
  */
 @Controller
-@RequestMapping("/system/performance/manager_kpi/view-manager-kpi-month-score")
-public class ManagerKpiScoreControllerOld {
+@RequestMapping("/system/performance/manager_kpi/view-manager-kpi-year-score")
+public class ManagerKpiYearScoreController {
     @Autowired
     private ManagerKpiScoreServiceOld managerKpiScoreServiceOld;
 
-
-
     /**
-     * excel 导出
-     * @param request 请求
-     * @param response 响应
+     * 经理人年度KPI分数管理界面
+     * @param model
+     * @return
      */
-    @RequiredLog("数据导出")
-    @GetMapping("downloadForCharge")
-    public void downloadForCharge(HttpServletRequest request, HttpServletResponse response) {
-        Map<String,Object> params = getParams(request);
-        //Sql语句
-        params.put("select","year,managerName,companyName,position,generalManager,month," +
-                "businessScore,incentiveScore,difficultyCoefficient,generalManagerScore,scoreAdjust,scoreAuto," +
-                "advantageAnalyze,disadvantageAnalyze,riskDesc,respDepartment,groupImproveAction");
-        //查询
-        List<Map<String,Object>> managerKpiScoreOldDataByParams = managerKpiScoreServiceOld.findManagerKpiScoreOldDataByParams(params);
-        LinkedHashMap<String,String> fieldMap = new LinkedHashMap<>();
-
-        //需要改变背景的格子
-        fieldMap.put("year", "年份");
-        fieldMap.put("managerName", "经理人姓名");
-        fieldMap.put("companyName", "企业名称");
-        fieldMap.put("position", "职务");
-        fieldMap.put("generalManager", "是否总经理");
-        fieldMap.put("month", "月份");
-        fieldMap.put("businessScore", "经营绩效分数");
-        fieldMap.put("incentiveScore", "激励约束分数");
-        fieldMap.put("difficultyCoefficient", "难度系数");
-        fieldMap.put("generalManagerScore", "总经理经营绩效分数");
-        fieldMap.put("scoreAuto", "系统生成分数");
-        fieldMap.put("scoreAdjust", "人工调整分数");
-        fieldMap.put("advantageAnalyze", "绩效优点");
-        fieldMap.put("disadvantageAnalyze", "绩效存在问题");
-        fieldMap.put("riskDesc", "绩效风险");
-        fieldMap.put("respDepartment", "总部管控责任部门");
-        fieldMap.put("groupImproveAction", "管控措施");
-        //标识字符串的列
-        List<Integer> strList = Arrays.asList(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
-        SheetOutputService sheetOutputService = new SheetOutputService();
-        if (org.apache.commons.lang3.ObjectUtils.isEmpty(managerKpiScoreOldDataByParams)) {
-            throw new ServiceException("未查出数据");
-        }
-        sheetOutputService.exportNum("经理人KPI分数表", managerKpiScoreOldDataByParams, fieldMap, response, strList, null);
+    @GetMapping("index")
+    @RequiresPermissions("system:performance:manager_kpi:view-manager-kpi-year-score:index")
+    @RequiredLog("经理人年度KPI分数")
+    public String manageIndex(Model model) {
+        Map<String, Object> params = new HashMap<>();
+        return "/system/performance/manager_kpi/view-manager-kpi-year-score/view-manager-kpi-year-score-list";
     }
-
+    /**
+     * 返回 经理人年度KPI分数数据表格
+     *
+     * @param request
+     * @return
+     */
+    @GetMapping("getYearScore")
+    @ResponseBody
+    public Map<String, Object> getViewManagerKpiMonthDatas(HttpServletRequest request) {
+        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> params = getParams(request);
+        int limit = Integer.parseInt(request.getParameter("limit"));
+        int page = Integer.parseInt(request.getParameter("page"));
+        params.put("limit", limit);
+        params.put("page", page);
+        Page<Map<String,Object>> managerKpiScorePage = managerKpiScoreServiceOld.findViewManagerKpiMonthDataListPerPageByParams(params);
+        if (managerKpiScorePage.getTotal() == 0) {
+            result.put("code", 404);
+            result.put("msg", "未查出数据");
+        } else {
+            result.put("code", 0);
+            result.put("count", managerKpiScorePage.getTotal());
+            result.put("data", managerKpiScorePage.getRecords());
+        }
+        return result;
+    }
     //获取数据库里的数据
     private Map<String, Object> getParams(HttpServletRequest request) {
         Map<String, Object> params = new HashMap<>();
@@ -154,9 +145,19 @@ public class ManagerKpiScoreControllerOld {
         if (!ObjectUtils.isEmpty(request.getParameter("groupImproveAction"))) {
             params.put("groupImproveAction", request.getParameter("groupImproveAction"));
         }
+        if (!ObjectUtils.isEmpty(request.getParameter("newYear"))) {
+            params.put("newYear", request.getParameter("newYear"));
+        }
+        if (!ObjectUtils.isEmpty(request.getParameter("oneYear"))) {
+            params.put("oneYear", request.getParameter("oneYear"));
+        }
+        if (!ObjectUtils.isEmpty(request.getParameter("twoYear"))) {
+            params.put("twoYear", request.getParameter("twoYear"));
+        }
+        if (!ObjectUtils.isEmpty(request.getParameter("threeYear"))) {
+            params.put("threeYear", request.getParameter("threeYear"));
+        }
         return params;
     }
-
-
 
 }
